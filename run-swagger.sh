8ec388918c005dc813270836a60dd7a0eb63dcc2
Kriss <kriss@vilanet.fr>
2017-05-11 11:08:13 +0200

    fix slash issue

--- run-swagger.sh
+++ run-swagger.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name swagger -d -p 127.0.0.1:8080:8080 dockerhub.vilanet.fr/kriss//swagger-ui:3.0.9
+docker run --name swagger -d -p 127.0.0.1:8080:8080 dockerhub.vilanet.fr/kriss/swagger-ui:3.0.9
